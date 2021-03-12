1ccf44d3dc3f739b136604ab21983d295f251bed
Marguerite Blair <marguerite.blair@gmail.com>
2021-03-11 22:04:45 -0500

    function to determine the directional flow a tuple

--- Tuples.hs
+++ Tuples.hs
@@ -6,4 +6,9 @@ main = do
     --to another exists within a directed graph
 
 hasPath :: [(Int, Int)] -> Int -> Int -> Bool
-hasPath [(a, b)] = False
+hasPath [] x y = x == y
+hasPath xs x y
+    | x == y = True
+    | otherwise =
+        let xs' = [ (n, m) | (n, m) <- xs, n /= x] in
+            or [ hasPath xs' m y | (n, m) <- xs, n == x]
