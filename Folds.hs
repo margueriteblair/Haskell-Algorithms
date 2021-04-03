7f5c79bc2d3f991e601d6200aa87a86b27315b69
Marguerite Blair <marguerite.blair@gmail.com>
2021-04-03 13:02:31 -0400

    foldr

---
+++ Folds.hs
@@ -0,0 +1,13 @@
+{-
+The foldr function associates the combining function to the right.
+The right most elements of the data structure will be evaluated first.
+
+-}
+
+import System.IO
+
+--the foldr function is defined as such:
+
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr f v [] = v
+foldr f v (x:xs) = f x (Main.foldr f v xs)
