11d69ae7dfc63d6821c1aae47e6f387afaf71a96
Marguerite Blair <marguerite.blair@gmail.com>
2021-04-03 14:36:02 -0400

    folds to calculate products

--- Folds.hs
+++ Folds.hs
@@ -6,6 +6,8 @@ The right most elements of the data structure will be evaluated first.
 
 import System.IO
 
+main = do
+    print (Main.product [1,2,3,4])
 --the foldr function is defined as such:
 
 foldr :: (a -> b -> b) -> b -> [a] -> b
@@ -14,3 +16,6 @@ foldr f v (x:xs) = f x (Main.foldr f v xs)
 
 sum :: Num a => [a] -> a
 sum xs = Prelude.foldr (+) 0 xs
+
+product :: Num a => [a] -> a
+product xs = Prelude.foldr (*) 1 xs
