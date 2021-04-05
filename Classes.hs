43a9765786678facd7389befaf3dbacd9f68c9a9
Marguerite Blair <marguerite.blair@gmail.com>
2021-04-05 15:34:08 -0400

    types of classes

---
+++ Classes.hs
@@ -0,0 +1,16 @@
+{- 
+We can think of classes as collections of types that will support operations of the class
+
+The basic type classes are the following:
+
+Eq - equality types
+Show - Showable types
+Num - Numeric types
+Fractional - fractional types (double & float)
+Ord - ordered types
+Read - Readable types
+Integral - Integral types
+Enum - enumeration types (like a suit on a deck of cards would be)
+
+-}
+
