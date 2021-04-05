f6d96f6ff27472eb64ec8c8f34c710dea392db78
Marguerite Blair <marguerite.blair@gmail.com>
2021-04-05 15:39:47 -0400

    classes

--- Classes.hs
+++ Classes.hs
@@ -21,3 +21,6 @@ Enum - enumeration types (like a suit on a deck of cards would be)
 
 --Char, Bool, Int, etc are instances of the Eq class, and that's why we can compare them
 
+--The Ord class requires any type that wants to be an instance of it to first be an instance of the Eq
+--class by using a class constraint, and to also support the >, <, <=, >=
+
