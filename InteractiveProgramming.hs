e75c06ee450cd69dedcad57ea6328d80cf098c30
Marguerite Blair <marguerite.blair@gmail.com>
2021-04-06 13:23:21 -0400

    input output

--- InteractiveProgramming.hs
+++ InteractiveProgramming.hs
@@ -11,4 +11,19 @@ where IO is the type name and a is the parameterised value that it returns
 
 IO Int -- is an action that returns an Int
 IO () is an action that returns an empty tuple
+
+IO () represents an action that is solely run for its side-effects and simply returns an empty tuple (a void value)
+
+Sometimes, we want to perform a sequence of actions one after the other.
+This can be done with 'do' notation in Hasekll, which is used to create one composite
+action from two or more individual actions in the do block. 
 -}
+
+main :: IO ()
+main = do
+    hello
+
+hello :: IO ()
+hello = do
+    putStrLn "Hello"
+    putStrLn "World!"
