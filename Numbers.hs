9877d00ebd35e84d5a6d204ecb7cdebda4434185
Marguerite Blair <marguerite.blair@gmail.com>
2021-04-06 13:29:54 -0400

    multiway if

---
+++ Numbers.hs
@@ -0,0 +1,38 @@
+{-
+We'll be making a guessing game to be played between two players.
+-}
+{-# LANGUAGE MultiWayIf #-}
+
+numbers :: IO ()
+numbers = do
+    putStrLn "Think of a number: "
+    number <- getSecretNumber
+    putStrLn "Guess the number: "
+    play number
+
+getInt :: IO Int
+getInt = do
+  number <- getLine
+  return (read number :: Int)
+
+getSecretNumber :: IO Int
+getSecretNumber = do
+    hSetEcho stdin False
+    number <- getInt
+    hSetEcho stdin True
+    return number
+
+play :: Int -> IO ()
+play number = do
+  putStr "? "
+  guess <- getInt
+  if | guess == number ->
+         putStrLn "That's correct!"
+     | guess > number ->
+         do
+           putStrLn "Too high!"
+           play number
+     | otherwise ->
+         do
+           putStrLn "Too low!"
+           play number
